ad7822b3ca39a850801c2e33d66afbd425d4be9d
Boris Pouw <74407773+BPouw@users.noreply.github.com>
2022-09-19 14:52:00 +0200

    Basics for login/register

--- Core.Domain/Person.cs
+++ Core.Domain/Person.cs
@@ -9,7 +9,8 @@ namespace Core.Domain
     public class Person
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Email { get; set; }
         public DateOnly DateOfBirth { get; set; }
         public enum Genders { M, V, X };
