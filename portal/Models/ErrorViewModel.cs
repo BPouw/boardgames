87ee8f58b25fb909f3954f2e5068a3adf0a65478
Boris Pouw <boris.pouw@cm.com>
2022-10-08 11:50:46 +0200

    New portal?

--- portal/Models/ErrorViewModel.cs
+++ portal/Models/ErrorViewModel.cs
@@ -1,9 +1,8 @@
-namespace portal.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
+namespace Portal.Models;
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
