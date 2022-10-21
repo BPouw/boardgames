32906c227b47add2ffc17ef1820dea44154c5a3e
boris.pouw <bj.pouw@student.avans.nl>
2022-10-21 10:19:16 +0200

    allergies built in

---
+++ Infrastructure/Migrations/20221021070646_v5.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class v5 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "GameNight",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateTime",
+                value: new DateTime(2022, 10, 21, 9, 6, 45, 932, DateTimeKind.Local).AddTicks(3410));
+
+            migrationBuilder.UpdateData(
+                table: "GameNight",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "DateTime",
+                value: new DateTime(2022, 10, 21, 9, 6, 45, 932, DateTimeKind.Local).AddTicks(3480));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "GameNight",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateTime",
+                value: new DateTime(2022, 10, 20, 22, 26, 40, 851, DateTimeKind.Local).AddTicks(4181));
+
+            migrationBuilder.UpdateData(
+                table: "GameNight",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "DateTime",
+                value: new DateTime(2022, 10, 20, 22, 26, 40, 851, DateTimeKind.Local).AddTicks(4227));
+        }
+    }
+}
